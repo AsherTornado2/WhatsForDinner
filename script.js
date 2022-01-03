735eac21684cc49c5127b518eb25b32ac9b297bf
AsherTornado2 <74437289+AsherTornado2@users.noreply.github.com>
2022-01-03 01:19:18 +0000

    added javascript folder

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-var x = document.getElementById("demo");
+//console.log
+var message = "Program is up and running"
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
+console.log(message);
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
+//
